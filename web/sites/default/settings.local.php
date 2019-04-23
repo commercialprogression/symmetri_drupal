f7fca0a296e4092148eaa22688e24bea52010d38
Hillary Lewandowski <hillary@commercialprogression.com>
2019-04-23 10:21:44 -0400

    Updating with the best local settings.php and services.yml files

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -1,11 +1,6 @@
 <?php
 
 /**
- * @file
- * Drupal site-specific configuration file.
- */
-
-/**
  * Local error reporting.
  */
 error_reporting(E_ALL);
@@ -17,31 +12,24 @@ $config['system.logging']['error_level'] = 'verbose';
 $config['system.performance']['css']['preprocess'] = FALSE;
 $config['system.performance']['js']['preprocess'] = FALSE;
 
-/**
- * Local other settings.
- */
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/local.services.yml';
 $settings['file_chmod_directory'] = 0755;
 $settings['file_chmod_file'] = 0644;
-$settings['hash_salt'] = 'V8xV4rh70SN0';
+$settings['hash_salt'] = 'Xi0pd_Tv7FllD-7VQDF8OMZlyoeMp5oUc2rOIZ55jUvUTOfkGlLUDo2QLzafzNQ1MqdIoGOWWg';
+$settings['install_profile'] = 'minimal';
 $settings['skip_permissions_hardening'] = TRUE;
+$settings['trusted_host_patterns'][] = '^localhost.*$';
+$settings['trusted_host_patterns'][] = '^127\.0.*$';
 
-/**
- * Databases.
- */
-$databases = array (
-  'default' =>
-  array (
-    'default' =>
-    array (
-      'database' => 'drupal',
-      'username' => 'root',
-      'password' => 'root',
-      'host' => 'mysql',
-      'port' => '',
-      'driver' => 'mysql',
-      'prefix' => '',
-    ),
-  ),
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'root',
+  'password' => 'root',
+  'prefix' => '',
+  'host' => 'mysql',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
 );
 
 $config['search_api.server.solr']['backend_config']['connector_config']['core'] = 'd8';
@@ -49,23 +37,5 @@ $config['search_api.server.solr']['backend_config']['connector_config']['path'] 
 $config['search_api.server.solr']['backend_config']['connector_config']['host'] = 'solr';
 $config['search_api.server.solr']['backend_config']['connector_config']['port'] = '8983';
 
-/**
- * General configs.
- */
-$conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
-$conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
-$conf['404_fast_paths_exclude'] = '/\/(?:styles)\//';
-$conf['theme_debug'] = '1';
-$drupal_hash_salt = 'kczJRAl6ByVYlbSVuiOTvt3zef3q6llP0nXiXQnGJCg';
-$update_free_access = FALSE;
-ini_set('session.cookie_lifetime', 2000000);
-ini_set('session.gc_divisor', 100);
-ini_set('session.gc_maxlifetime', 200000);
-ini_set('session.gc_probability', 1);
-
-$settings['trusted_host_patterns'] = [
-  'localhost',
-];
-
-$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/development.services.yml';
-$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/local.services.yml';
+assert_options(ASSERT_ACTIVE, TRUE);
+\Drupal\Component\Assertion\Handle::register();
